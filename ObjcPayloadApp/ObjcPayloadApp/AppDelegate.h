4cc4addcae176f88b1ab15417424c67b4fb616bd
oonoyosp <oonoyosp@pck-133-330.local>
2016-10-25 09:42:44 +0900

    first setup

---
+++ ObjcPayloadApp/ObjcPayloadApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcPayloadApp
+//
+//  Created by oono on 2016/10/25.
+//  Copyright © 2016年 Nifty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
